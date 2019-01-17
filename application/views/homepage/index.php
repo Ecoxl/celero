c6a50a1275fb0053fe4f3ac925e87b28d27b971d
basil <basil.gisi@gmail.com>
2019-01-17 10:39:24 +0100

    first try of a new home image

--- application/views/homepage/index.php
+++ application/views/homepage/index.php
@@ -1,10 +1,10 @@
 <div class="homemain" style="margin-top: -30px;">
 	<div class="container text-center">
 		<br>
-		<div style="color: white;font-size: 28px;padding: 10px;margin-top: 60px;"><?php echo lang('slogan'); ?></div>
+		<div style="color: black;font-size: 28px;padding: 10px;margin-top: -30px;"><?php echo lang('slogan'); ?></div>
 		<?php if ($this->session->userdata('user_in') == FALSE): ?>
-			<div style="margin-top:50px;">
-				<a class="btn btn-lg btn-success" href="<?php echo base_url('register'); ?>"><?php echo lang("startusing"); ?></a>
+			<div style="margin-top:450px;">
+				<a class="btn btn-lg btn-success" style="font-size: 15px;padding: 10px 40px;" href="<?php echo base_url('register'); ?>"><?php echo lang("startusing"); ?></a>
 			</div>
 		<?php endif ?>
 	</div>
