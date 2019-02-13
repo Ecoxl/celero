5f412ea5d834e614f97ebfb9ddbd8e76104f64a7
basil <basil.gisi@gmail.com>
2019-02-13 16:48:21 +0100

    improved disclaimer text

--- application/views/pages/legal.php
+++ application/views/pages/legal.php
@@ -6,13 +6,14 @@
 		<h><b>Legal disclaimer</b></h></p>
 		<p>
 			<b>Data Protection</b><br>
-			Correct and up-to-date information are important for the FHNW. It is therefore possible that some or all content on our website may be partially amended, deleted or is (temporarily) no longer published.
+			Correct and up-to-date information is important for the FHNW. It is therefore possible that some or all content on our website may be partially amended, deleted or temporarily unavailable.
 			<br>
 			<br>
 
 			<b>Access to the CELERO website</b><br>
-			We take protection of data seriously. The access to the CELERO website is therefore only possible via login.  The users are aware that they work with confidential data within CELERO.
-			The storage of data is under the sovereignty of the webhosting provider, who has its own data protection declaration.
+			We take data protection seriously. Access to the CELERO website is therefore only possible via login.  Users are aware that they work with confidential data within CELERO.
+			Data storage is governed by the webhosting provider, who has their own data protection declaration.
+
 			<br>
 			<br>
 
@@ -21,28 +22,30 @@
 			We never disclose personal data to third parties, unless the law requires us to do so or we have obtained an explicit permission.
 			The use of the services which CELERO offers requires the creation of a user or company profile.
 			The profiles which are created by registered users contain personal data. For collaboration on projects the data can be visible for other users.
+
 			<br>
 			<br>
 
 			<b>Company data</b><br>
-			Within CELERO its possible to store material and energy flow (amount, cost, environmental impact) data on processes and best practices.
-			They are used to provide estimations of optimisation potentials and to develop measures.
-			The data will be used for statistical analysis. The evaluations are used towards the Swiss Federal Office for the Environment (FOEN) as a success monitoring of CELERO.
+			Within CELERO it is possible to store material and energy flow (amount, cost, environmental impact) data on processes and best practices.
+			They are used to provide estimations of optimisation potential and to develop methods.
+			The data will be used for statistical analysis. The evaluations are used by the Swiss Federal Office for the Environment (FOEN) for success monitoring of CELERO.
 			<br>
 			<br>
 
 			<b>Cookies</b><br>
 			Cookies are text files which are stored temporarily or permanently on your computer and, for example, enable your use of the website to be analysed.
-			You can entirely or partially prevent cookies from being stored on your computer at any time by adjusting the corresponding settings in your browser software. However, please be aware that, if you do so, you may no longer be able to use all of the functions on this website.
+			You can entirely or partially prevent cookies from being stored on your computer at any time by adjusting the corresponding settings in your browser software. However, please be aware that if you do so you may no longer be able to use all of the functions on this website.
+
 			<br>
 			<br>
  
-			You have the possibility to deactivate cookies and web analysis methods inside your own browser’s configuration page. The manuals to do this can be found either on the site of the browser supplier or on different documentation on the internet, for example under <a href="https://www.datenschutz.ch" target="_blank" rel="noopener noreferrer">www.datenschutz.ch</a>
+			You have the possibility to deactivate cookies and web analysis methods inside your own browser’s configuration page. The manuals to do this can be found either on the site of the browser supplier or in documentation on the internet, for example on <a href="https://www.datenschutz.ch" target="_blank" rel="noopener noreferrer">www.datenschutz.ch</a>
 			<br>
 			<br>
 
 			<b>Disclaimer</b><br>
-			FHNW shall not accept any liability for any material or immaterial damage arising in connection with the use or non-use of the information offered, with the misuse of the connections or with technical faults.
+			FHNW does not accept any liability for any material or immaterial damage arising in connection with the use or non-use of the information offered, with the misuse of the connections or with technical faults.
 		</p>
 	</div>
 </div>
