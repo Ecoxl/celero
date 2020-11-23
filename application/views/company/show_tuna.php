3e6928ee54c35140a5430d83b987773761208185
basil <basil.gisi@gmail.com>
2020-11-23 08:44:27 +0100

    correct https url for leaflet js maps

--- application/views/company/show_tuna.php
+++ application/views/company/show_tuna.php
@@ -49,8 +49,8 @@
     <div class="col-md-5">
         <div class="swissheader"><?php echo $cluster_name['name'];?></div>
         <!-- harita -->
-        <link rel="stylesheet" href="https://cdn.leafletjs.com/leaflet-0.7.3/leaflet.css" />
-        <script src="https://cdn.leafletjs.com/leaflet-0.7.3/leaflet.js"></script>
+        <link rel="stylesheet" href="https://unpkg.com/leaflet@0.7.3/dist/leaflet.css" />
+        <script src="https://unpkg.com/leaflet@0.7.3/dist/leaflet.js"></script>
         <?php
         $company_array = array();
         foreach ($companies as $com => $k) {
